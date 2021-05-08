2f1401d5a3f1d782bf5a892e462d554cb4e4bfdc
Anthony dela Calzada <anthony.delacalzada@gmail.com>
2021-05-07 18:04:29 -0700

    Grouped cart items to update item quantity

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,16 @@
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+    const existiingCartItem = cartItems.find(
+        cartItem => cartItem.id === cartItemToAdd.id
+    );
+
+    if(existiingCartItem){
+        return cartItems.map(cartItem =>
+            cartItem.id === cartItemToAdd.id
+            ? { ...cartItem, quantity: cartItem.quantity + 1 }
+            : cartItem
+            )
+    }
+
+    // First time adding this item:
+    return [...cartItems, { ...cartItemToAdd, quantity: 1}]
+}
