16e287c42e7fc6d0623fa2bea443587e665be2c6
Anthony dela Calzada <anthony.delacalzada@gmail.com>
2021-05-10 15:43:27 -0700

    Nested Routing in Shop Page for Collections aka Categories

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+
 import CollectionOverview from '../../components/collections-overview/collections-overview.component';
+
+import CategoryPage from '../collection/collection.component';
 
 // import Directory from '../../components/directory/directory.component';
 // import './homepage.styles.scss';
 
-const ShopPage = ({collections}) => (
+const ShopPage = ({ match }) => (
     <div className='shop-page'>
-            <CollectionOverview />
+            <Route exact path={`${match.path}`} component={CollectionOverview} />
+            <Route path={`${match.path}/:collectionId`} component={CategoryPage} />
     </div>
 );
 
