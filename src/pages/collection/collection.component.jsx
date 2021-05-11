3e53a0cfaca2fce0ac0c6ff2b2bcde4108039dcf
Anthony dela Calzada <anthony.delacalzada@gmail.com>
2021-05-11 12:00:03 -0700

    Data Normalization on collections and updates

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import CollectionItem from '../../components/collection-item/collection-item.component';
 
 import { selectCollection } from '../../redux/shop/shop.selectors';
 
 import './collection.styles.scss';
 
-const CategoryPage = ({ collection }) => (
-    <div className='category-page' name={collection.id}>
-        <h2>CATEGORY PAGE</h2>
-    </div>
-);
+const CategoryPage = ({ collection }) => {
+    const { title, items } = collection;
+    return(
+        <div className='category-page'>
+            <h2 className='title'>{title}</h2>
+            <div className='items'>
+                {
+                    items.map(item => <CollectionItem key={item.id} item={item} />)
+                }
+            </div>
+        </div>
+    );
+};
 
 const mapStateToProps = (state, ownProps) => ({
     collection: selectCollection(ownProps.match.params.collectionId)(state)
