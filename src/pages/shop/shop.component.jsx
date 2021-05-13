3df9867668cc8b04bb2c2f81842f29f7bcb22e16
Anthony dela Calzada <anthony.delacalzada@gmail.com>
2021-05-13 08:37:03 -0700

    WithSpinner HOC to Collections/Category

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -5,13 +5,22 @@ import { connect } from 'react-redux';
 import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
 import { updateCollections } from '../../redux/shop/shop.actions';
 
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
+
 import CollectionOverview from '../../components/collections-overview/collections-overview.component';
 import CategoryPage from '../collection/collection.component';
 
 // import Directory from '../../components/directory/directory.component';
 // import './homepage.styles.scss';
 
+const CollectionOverviewWithSpinner = WithSpinner(CollectionOverview);
+const CategoryPageWithSpinner = WithSpinner(CategoryPage);
+
 class ShopPage extends React.Component{
+    state = {
+        loading: true
+    }
+
     unsubscribeFromSnapshott = null;
 
     componentDidMount() {
@@ -21,16 +30,22 @@ class ShopPage extends React.Component{
         this.unsubscribeFromSnapshott = collectionRef.onSnapshot(async snapshot => {
             const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
             updateCollections(collectionsMap);
+            this.setState({loading:false});
         });
     }
 
     render(){
         const { match } = this.props;
+        const { loading } = this.state;
         return (
             <div className='shop-page'>
-            <Route exact path={`${match.path}`} component={CollectionOverview} />
-            <Route path={`${match.path}/:collectionId`} component={CategoryPage} />
-    </div>
+                <Route exact path={`${match.path}`} 
+                    render={(props) => <CollectionOverviewWithSpinner isLoading={loading} {...props} /> }
+                />
+                <Route path={`${match.path}/:collectionId`}
+                    render={(props) => <CategoryPageWithSpinner isLoading={loading} {...props} /> }
+                />
+            </div>
         );
     }
 };
