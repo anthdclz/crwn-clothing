83ead3394a53c7517c9ee1c92cff9c048a4dee30
Anthony dela Calzada <anthony.delacalzada@gmail.com>
2021-05-12 17:53:21 -0700

    Pull shop data from Firebase

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,18 +1,42 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+import { updateCollections } from '../../redux/shop/shop.actions';
 
 import CollectionOverview from '../../components/collections-overview/collections-overview.component';
-
 import CategoryPage from '../collection/collection.component';
 
 // import Directory from '../../components/directory/directory.component';
 // import './homepage.styles.scss';
 
-const ShopPage = ({ match }) => (
-    <div className='shop-page'>
+class ShopPage extends React.Component{
+    unsubscribeFromSnapshott = null;
+
+    componentDidMount() {
+        const { updateCollections}  = this.props;
+        const collectionRef = firestore.collection('collections');
+
+        this.unsubscribeFromSnapshott = collectionRef.onSnapshot(async snapshot => {
+            const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+            updateCollections(collectionsMap);
+        });
+    }
+
+    render(){
+        const { match } = this.props;
+        return (
+            <div className='shop-page'>
             <Route exact path={`${match.path}`} component={CollectionOverview} />
             <Route path={`${match.path}/:collectionId`} component={CategoryPage} />
     </div>
-);
+        );
+    }
+};
 
-export default ShopPage;
+const mapDispatchToProps = dispatch => ({
+    updateCollections: collectionsMap => dispatch(updateCollections(collectionsMap))
+});
+
+export default connect(null, mapDispatchToProps)(ShopPage);
---
+++ src/redux/shop/shop.types.js
@@ -0,0 +1,5 @@
+const ShopActionTypes = {
+    UPDATE_COLLECTIOONS: 'UPDATE_COLLECTIONS'
+};
+
+export default ShopActionTypes;
